4c4b5ada1cfcf307a38e3b308c0fbb1255bf85bc
hakngrow <hakngrow@gmail.com>
2021-11-13 16:31:01 +0800

    Initial commit

---
+++ app/config/db.config.js
@@ -0,0 +1,7 @@
+
+console.log(process.env.DB_USER, process.env.DB_PASSWORD);
+
+
+module.exports = {
+  url: "mongodb+srv://db_user:EbGRGrToP6dmImys@cluster0.cp50y.mongodb.net/misc_db?retryWrites=true&w=majority",
+};
---
+++ server.js
@@ -0,0 +1,47 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081",
+};
+
+app.use(cors(corsOptions));
+
+// Parse requests of content-type - application/json
+app.use(express.json());
+
+// Parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// Connect to database - Mongo Atlas
+const db = require("./app/models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch((err) => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
+// Testing route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+// API routes
+require("./app/routes/tutorial.routes")(app);
+
+// Set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
