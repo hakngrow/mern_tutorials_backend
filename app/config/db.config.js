9815e78a8cf5b8eebaa272020254c004e04411d4
hakngrow <hakngrow@gmail.com>
2021-11-15 10:59:23 +0800

    Created test.jpg

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,4 @@
 
-console.log(process.env.DB_USER, process.env.DB_PASSWORD);
-
-
 module.exports = {
   url: "mongodb+srv://db_user:EbGRGrToP6dmImys@cluster0.cp50y.mongodb.net/misc_db?retryWrites=true&w=majority",
 };
